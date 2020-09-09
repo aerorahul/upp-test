b27b5a5639c63e3fc1789f271a03223fb48c27df
Rahul Mahajan <aerorahul@users.noreply.github.com>
2020-09-09 12:36:20 -0400

    Update upp_compiler_flags.cmake

--- cmake/upp_compiler_flags.cmake
+++ cmake/upp_compiler_flags.cmake
@@ -1,14 +1,14 @@
 if(CMAKE_Fortran_COMPILER_ID MATCHES "^(Intel)$")
 
   set(CMAKE_Fortran_FLAGS
-      "-g -traceback -fp-model source -free -convert big_endian")
+      "${CMAKE_Fortran_FLAGS} -g -traceback -fp-model source -free -convert big_endian")
   set(CMAKE_Fortran_FLAGS_RELEASE "-O3")
   set(CMAKE_Fortran_FLAGS_DEBUG "-O0")
 
-elseif(CMAKE_Fortran_COMPILER_ID MATCHES "^(GNU|Clang|AppleClang)$")
+elseif(CMAKE_Fortran_COMPILER_ID MATCHES "^(GNU)$")
 
   set(CMAKE_Fortran_FLAGS
-      "-g -fbacktrace -ffree-form -ffree-line-length-none -fconvert=big-endian")
+      "${CMAKE_Fortran_FLAGS} -g -fbacktrace -ffree-form -ffree-line-length-none -fconvert=big-endian")
   set(CMAKE_Fortran_FLAGS_RELEASE "-O3")
   set(CMAKE_Fortran_FLAGS_DEBUG "-O0 -ggdb -fno-unsafe-math-optimizations -frounding-math -fsignaling-nans -ffpe-trap=invalid,zero,overflow -fbounds-check")
 
